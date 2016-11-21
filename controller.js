ea62c1754e13907965f3b3d152d076b8ad753615
Colin Walbridge <cwalbrid@umich.edu>
2016-11-21 12:57:25 -0500

    initial commit

---
+++ controller.js
@@ -0,0 +1,42 @@
+
+var app = angular.module("kidTalk", []);
+
+app.controller('kidTalkController', function($scope){
+	$scope.kidSaysThis = [];
+	$scope.words = ['no', 'me', 'I', 'gimme', 'cookie', 'candy', 'toy', 'mine', 'hungry', 'angry'];
+	$scope.dec = ['classA', 'classB', 'classC', 'classD', 'classE'];
+
+	$scope.phrases = ['my turn', 'go away', 'no fair', 'watch this', 'look out', 'that hurts', 'gimme that', 'no way', 'you stink', 'move over'];
+	
+	
+	$scope.randomWord = function(){
+		var item = {};
+		item.kidSaysThis= $scope.words[Math.floor(Math.random() * $scope.words.length)] + "! ";
+		$scope.kidSaysThis.push(item);
+		console.log(item);
+	};
+	
+	$scope.randomPhrase = function(){
+		var item = {};
+		item.kidSaysThis= $scope.phrases[Math.floor(Math.random() * $scope.phrases.length)] + "! ";
+		$scope.kidSaysThis.push(item);
+		console.log(item);
+	};
+
+	$scope.randomFancyWord = function(){
+		var item = {};
+		item.kidSaysThis= $scope.words[Math.floor(Math.random() * $scope.words.length)] + "! ";
+		item.decoration = $scope.dec[Math.floor(Math.random() * $scope.dec.length)];
+		$scope.kidSaysThis.push(item);
+		console.log(item);
+	};
+	
+	$scope.randomFancyPhrase = function(){
+		var item = {};
+		item.kidSaysThis= $scope.phrases[Math.floor(Math.random() * $scope.phrases.length)] + "! ";
+		item.decoration = $scope.dec[Math.floor(Math.random() * $scope.dec.length)];
+		$scope.kidSaysThis.push(item);
+		console.log(item);
+	};
+});
+
